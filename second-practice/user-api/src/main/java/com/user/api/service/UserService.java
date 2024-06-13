f072f426413f8338562e5a386a365a0473e65985
hku2002 <hku2002@hanmail.net>
2024-06-13 23:31:00 +0900

    feat: user 명 조회 로직 추가

--- second-practice/user-api/src/main/java/com/user/api/service/UserService.java
+++ second-practice/user-api/src/main/java/com/user/api/service/UserService.java
@@ -1,12 +1,19 @@
 package com.user.api.service;
 
+import com.user.entity.User;
+import com.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
     public String findUserById(Long id) {
-        return "";
+        User user = userRepository.findById(1L).orElseThrow();
+        return user.getName();
     }
 
 }
