ee19cbef95e69faf81dfb4d469184bb6fe4cbde0
hku2002 <hku2002@hanmail.net>
2024-06-14 21:48:05 +0900

    feat: spring batch 로직 추가

---
+++ second-practice/user-batch/src/main/java/com/user/batch/job/grade/GradeUpgradeConfig.java
@@ -0,0 +1,36 @@
+package com.user.batch.job.grade;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class GradeUpgradeConfig {
+
+    @Bean
+    public Job userUpgradeJob(JobRepository jobRepository, Step userUpgradeStep) {
+        return new JobBuilder("userUpgradeJob", jobRepository)
+                .start(userUpgradeStep)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step userUpgradeStep(JobRepository jobRepository
+            , PlatformTransactionManager transactionManager) {
+        return new StepBuilder("userUpgradeStep", jobRepository)
+                .chunk(10, transactionManager)
+                .build();
+    }
+
+}
